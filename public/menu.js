528d1f5e2ffd017535795ebf373d883b40e6071b
Cindy Siu <cindy@Cindys-MBP-2.fios-router.home>
2020-04-27 20:47:03 -0400

    Added submenu of schedules

--- public/menu.js
+++ public/menu.js
@@ -6,3 +6,11 @@ $(document).ready(function(){
         $('.menu').animate({right: '-50%'}, 100)
 });
 });
+
+function showMenu() {
+    var subMenu = document.querySelector('.sub-menu');
+    if (subMenu.style.display === "none") 
+        subMenu.style.display = "block";
+    else 
+        subMenu.style.display = "none";
+}
