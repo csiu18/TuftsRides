7c1c9366571faa5d446d60d0e4e273c2bc53b313
ankoanju <51080658+ankoanju@users.noreply.github.com>
2020-04-29 15:54:37 +0900

    js file for cookies

---
+++ public/cookies.js
@@ -0,0 +1,56 @@
+
+
+function initializeCookies()
+{
+	localStorage.setItem("CAMPUS CENTER FRONT", "FALSE");
+	localStorage.setItem("DAVIS SQUARE", "FALSE");
+	localStorage.setItem("CAMPUS CENTER BACK", "FALSE");
+	localStorage.setItem("CARMICHAEL HALL", "FALSE");
+	localStorage.setItem("OLIN HALL", "FALSE");
+	localStorage.setItem("AIDEKMANN", "FALSE");
+
+}
+
+function setCookie(name, value)
+{
+	console.log("set cookie" + name + "to" + value)
+	localStorage.setItem(name, value);
+
+}
+
+function removeCookie(name)
+{
+	localStorage.removeItem(name)
+}
+
+function getCookie(name)
+{
+	return localStorage.getItem(name)
+
+}
+
+function cookieLogic(name)
+{
+	var currentCookie = getCookie(name);
+	if(currentCookie == "FALSE"){
+		setCookie(name, "TRUE");
+	}else{
+		setCookie(name, "FALSE");
+	}
+}
+
+function getFavArray()
+{
+	var favs = [];
+	var nameArray = ["CAMPUS CENTER FRONT", "DAVIS SQUARE", "CAMPUS CENTER BACK",
+					"CARMICHAEL HALL", "OLIN HALL", "AIDEKMANN"];
+	for(var i = 0; i < 7; i++) {
+		if(getCookie(nameArray[i]) == "TRUE") {
+			console.log(nameArray[i])
+			favs.push(nameArray[i]);
+		}
+	}
+
+	return favs;
+}
+
