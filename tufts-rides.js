f3260f3eac41f8bda1a4a8df3270a63ab022b95a
Cindy Siu <cindy@Cindys-MBP-2.fios-router.home>
2020-05-02 02:34:37 -0400

    fixed day to friday for presentation

--- tufts-rides.js
+++ tufts-rides.js
@@ -24,6 +24,7 @@ async function startServer() {
 async function getTime(stopName) {
     var today = new Date();
     var day = today.getDay();
+    day = 5;
     var hour = today.getHours();
     var min = today.getMinutes();
     var index = 0;
